aeca40b68037c29239a5afdb9cd7a1f44544efd0
cortlandniccum <cortlandniccum@gmail.com>
2021-07-15 01:58:00 -0600

    finished assignment

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ const App = () => {
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
   const [posts, setPosts] = useState(dummyData);
+  const [searchTerm, setsearchTerm] = useState("");
   const likePost = (postId) => {
     console.log(postId);
     /*
@@ -31,12 +32,16 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    const newPosts = posts.map((post) =>
+      post.id === postId ? { ...post, likes: post.likes + 1 } : post
+    );
+    setPosts(newPosts);
   };
 
   return (
     <div className="App">
       {/* Add SearchBar and Posts here to render them */}
-      <SearchBar />
+      <SearchBar setsearchTerm={setsearchTerm} />
       <Posts likePost={likePost} posts={posts} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
